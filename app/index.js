40e12a9f6a0d30b143b1b37bfc9d3af344a59d00
dosemwengie <thewiz1906@gmail.com>
2018-02-04 21:23:03 -0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,40 +2,34 @@ var express=require('express');
 var app=express();
 var https=require('https');
 const axios=require('axios');
-/*var mongoose=require('mongoose');
-mongoose.Promise=global.Promise;
-mongoose.set('debug',true);
-const axios = require('axios');
-var cmschema = require('./lib/coinmarketschema.js');
-var db = cmschema.db;
-var coinmodel = cmschema.coinModel;
-db.on('error', console.error.bind(console, "DB ERROR"));
-*/
 var MongoClient = require('mongodb').MongoClient;
 var dblink = "mongodb://localhost:27017/"
 coinURL='https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=0';
 var collection="coinmarket";
 var userTable = "UserCoins";
-MongoClient.connect(dblink, function(err, database){
+var volumeTable = "volume";
+var processData = {};
+var mongoConnect = MongoClient.connect(dblink, callback);
+var processData  = function(err, database){
 	if (err) throw err;
 	var db = database.db("localhost");
 	axios.get(coinURL)
 		.then(function(response) {
 			var coins = response.data;
-			var getData = function(coin){ 
+			var getData = function(coin){
 				db.collection(collection).updateOne({name:coin.id},{ $set : {
-				name:coin.id, 
-				symbol: coin.symbol, 
-				rank: coin.rank, 
-				price_usd: coin.price_usd, 
-				price_btc: coin.price_btc, 
-				day_volume_usd: coin['24h_volume.usd'], 
-				market_cap_usd: coin.market_cap_usd, 
-				available_supply: coin.available_supply, 
-				total_supply: coin.total_supply, 
-				percent_change_1h:coin.total_supply_1h, 
-				percent_change_24h: coin.percent_change_24h, 
-				percent_change_7d: coin.percent_change_7d, 
+				name:coin.id,
+				symbol: coin.symbol,
+				rank: coin.rank,
+				price_usd: coin.price_usd,
+				price_btc: coin.price_btc,
+				day_volume_usd: coin['24h_volume.usd'],
+				market_cap_usd: coin.market_cap_usd,
+				available_supply: coin.available_supply,
+				total_supply: coin.total_supply,
+				percent_change_1h:coin.total_supply_1h,
+				percent_change_24h: coin.percent_change_24h,
+				percent_change_7d: coin.percent_change_7d,
 				last_updated: coin.last_updated }},
 				{ upsert: true }, function(err,res){
 				console.log(res.result);
@@ -49,7 +43,15 @@ MongoClient.connect(dblink, function(err, database){
 		});
 		});
 		});
-});
+	};
+var updateVolume = function(err, database){
+	if (err) throw err;
+	var db = database.db("localhost");
+	db.collection(collection).find({}, function(err,res){
 		
-		
-	
+		db.collection(volumeTable).updateOne({name: res.name}, {$set: 
+			date: res.last_updated,
+			volume: res.day_volume_usd
+		});
+	});
+};
