34975212f5186806fc3728610ff1bd0689401ced
dosemwengie <thewiz1906@gmail.com>
2018-07-26 23:58:52 -0500

    remove root logging

--- aws/lambda/aws_nmc.py
+++ aws/lambda/aws_nmc.py
@@ -13,6 +13,10 @@ from time import sleep
 
 getcontext().prec = 7
 
+root = logging.getLogger()
+if root.handlers:
+    for handler in root.handlers:
+        root.removeHandler(handler)
 logging.basicConfig(format='%(asctime)s %(message)s',level=logging.INFO)
 
 user_example = {'preference': {
